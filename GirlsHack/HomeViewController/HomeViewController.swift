b39d69f378c008cf8ecb4245adccc5bf7726e003
Niki Mirai <s1411400@coins.tsukuba.ac.jp>
2017-12-03 01:01:57 +0900

    Home画面TimeLine,Mapの追加

--- GirlsHack/HomeViewController/HomeViewController.swift
+++ GirlsHack/HomeViewController/HomeViewController.swift
@@ -7,13 +7,45 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class HomeViewController: UIViewController {
+class HomeViewController: ButtonBarPagerTabStripViewController {
 
     override func viewDidLoad() {
+        settings.style.buttonBarBackgroundColor = .white
+        // ButtonBarItemの背景色
+        settings.style.buttonBarItemBackgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        // 選択中のButtonBarの下部の色
+        settings.style.selectedBarBackgroundColor = .orange
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            // 選択されていないボタンのテキスト色
+            oldCell?.label.textColor = .black
+            // 選択されているボタンのテキスト色
+            newCell?.label.textColor = .yellow
+        }
+
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    /// ViewControllers
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+
+        // 追加したViewControllerを指定
+        let firstVC = UIStoryboard(name: "HomeViewController", bundle: nil).instantiateViewController(withIdentifier: "HomeTimeLine")
+        let secondVC = UIStoryboard(name: "HomeViewController", bundle: nil).instantiateViewController(withIdentifier: "HomeMap")
+
+        // ViewControllersに入れる
+        let childViewControllers:[UIViewController] = [firstVC, secondVC]
+        return childViewControllers
     }
 
     override func didReceiveMemoryWarning() {
